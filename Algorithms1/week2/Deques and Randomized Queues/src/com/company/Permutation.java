73d0d1006f4fc83afca39e477d2208df1b3383eb
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-10-03 03:33:56 +0200

    Permutation

---
+++ Algorithms1/week2/Deques and Randomized Queues/src/com/company/Permutation.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import edu.princeton.cs.algs4.StdIn;
+
+
+public class Permutation {
+    public static void main(String[] args) {
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
+        int k = Integer.parseInt(args[0]);
+        while (!StdIn.isEmpty()) {
+            randomizedQueue.enqueue(StdIn.readString());
+        }
+        for (int i = 0; i < k; i++) {
+            System.out.println(randomizedQueue.dequeue());
+        }
+    }
+}
